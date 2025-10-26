70bcce6ceafd1f44ac29e4e72fe6fdbf0156bd38
agazabonromero-bit <agazabonromero@gmail.com>
2025-10-26 00:53:34 -0500

    Fix base path in vite.config.js to solve MIME issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚙️ Configuración correcta para Vercel:
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 rutas relativas (evita error MIME)
+  build: {
+    outDir: 'dist',
+  },
 })
