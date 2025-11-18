5c7e5f21281558777a3c337aa2fc38f4332e6e49
agazabonromero-bit <agazabonromero@gmail.com>
2025-11-17 20:17:34 -0500

    fix: actualizar socket URL para producción

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 export const socket = io(URL, {
-  transports: ["websocket", "polling"],
+  transports: ["websocket"],
   withCredentials: true,
 });
 
