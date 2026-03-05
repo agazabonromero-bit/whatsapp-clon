f8da24bcf48e917fa5846700fe19c26813d8b04b
agazabonromero-bit <agazabonromero@gmail.com>
2026-03-04 22:11:30 -0500

    Fix socket connection

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,11 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+const URL =
+  import.meta.env.VITE_SOCKET_URL ||
+  "https://whatsapp-cloner-backend.onrender.com";
 
 export const socket = io(URL, {
-  transports: ["polling", "websocket"], 
+  transports: ["websocket", "polling"],
   withCredentials: true,
 });
 
