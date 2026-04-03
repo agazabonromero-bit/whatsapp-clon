568ccd975996df4065622a25e11badd1a4344ef9
agazabonromero-bit <agazabonromero@gmail.com>
2026-04-03 10:21:22 -0500

    debug socket url en producción

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
+console.log("🌐 SOCKET URL:", import.meta.env.VITE_SOCKET_URL);
+
 const URL =
   import.meta.env.VITE_SOCKET_URL ||
   "https://whatsapp-cloner-backend.onrender.com";
